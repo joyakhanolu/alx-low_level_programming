9cf33234d20a7ac9831bee5feae68e14a1c60a71
joyakhanolu <joyakhanolu17@gmail.com>
2022-03-10 11:48:29 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *#include <stdio.h> - generates output defined in stdio.h>
+ *
+ * int main()- the main entry point to the program.
+ *
+ * return(0) - terminates the main function and returns the value 0 to the calling process(0-executed successfully)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
