9f03ffdc8fc9454615df000f2ca496a3bf7be58f
joyakhanolu <joyakhanolu17@gmail.com>
2022-03-10 11:58:50 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Description: prints "with proper gtammer, but the outcome is a piece of art,"
+ * Return(0)- A success.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
