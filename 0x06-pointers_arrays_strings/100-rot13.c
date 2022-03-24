0591794622406044efa013b037187d214c32567b
joyakhanolu <joyakhanolu17@gmail.com>
2022-03-24 13:01:45 -0700

    Encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *rot13 - encodes a string using rot13
+ * @str: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int k;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (k = 0; k < 52; k++)
+		{
+			if (str[i] == data1[k])
+			{
+				str[i] = datarot[k];
+				break;
+			}
+		}
+	}
+	return (str);
+}
