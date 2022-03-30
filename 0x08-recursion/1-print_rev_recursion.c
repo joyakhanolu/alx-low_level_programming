e16134d1879f4f88ec58e61f9de9346a953e2735
Joy Akhanolu <66342188+joyakhanolu@users.noreply.github.com>
2022-03-30 03:45:46 -0700

    prints a string in reverse.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,11 +4,11 @@
  * _print_rev_recursion - prints a string in reverse
  * *s: string to be reversed
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *a)
 {
-	if (*s)
+	if (*a)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_print_rev_recursion(a + 1);
+		_putchar(*a);
 	}
 }
