afa5c33dccb68b3f9b1ecd40644121f8a1414c30
Joy Akhanolu <66342188+joyakhanolu@users.noreply.github.com>
2022-03-30 03:49:50 -0700

    prints strings in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * *s: string to be reversed
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-void _print_rev_recursion(char *a)
+void _print_rev_recursion(char *s)
 {
-	if (*a)
+	if (*s)
 	{
-		_print_rev_recursion(a + 1);
-		_putchar(*a);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
