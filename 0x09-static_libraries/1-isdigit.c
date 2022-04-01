ad184517f6101c48613b0e29a01721c1b408231c
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-04-01 13:11:33 -0700

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c - carrier variable
+ * Return: 1 if true, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
