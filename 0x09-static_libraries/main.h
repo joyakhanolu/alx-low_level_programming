2a26de8c5e44808f0728a4865f9d5a665167158a
joyakhanolu <joyakhanolu17@gmail.com>
2022-04-01 14:39:30 -0700

    Update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description: Header file contain all prototypes of all functions
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +25,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
