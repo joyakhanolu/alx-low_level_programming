71e906f7225222da9a09b408a1e82bd74fa15685
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-04-13 04:42:35 -0700

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,20 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @size - number of elements in the array
- * @cmp - pointer to the function to be used to compare values
- * Return: -1 if no element matches
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
  */
-int int_index(int *array, int size, int (*cmp)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int index;
+	if (array == NULL || action == NULL)
+		return;
 
-	if (array != NULL && cmp != NULL)
-		return (-1);
-
-	for (index = 0; index < size; index++)
+	while (size-- > 0)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
+		action(*array);
+		array++;
 	}
-
-	return (-1);
 }
