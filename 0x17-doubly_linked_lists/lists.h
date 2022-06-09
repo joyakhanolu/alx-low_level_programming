65e135762416d057f9d4ce573c6efb865f60960b
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-09 07:34:18 -0700

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,17 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* 
- * Desc: Header file containing prototypes for all functions
- * written in 0x17-doubly_linked_lists.
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int _putchar(char c);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
