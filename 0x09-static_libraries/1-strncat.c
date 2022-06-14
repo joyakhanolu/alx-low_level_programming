655d6475d234d423ced7be07cb9411fca885bec8
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-14 06:00:39 -0700

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
 /**
- * _isdigit - checks if a character is digit
- * @c - carrier variable
- * Return: 1 if true, otherwise 0
- */
-
-int _isdigit(int c)
+*_strncat - concatenate two strings but add inputted number of bytes
+*@dest: string to be appended upon
+*@src: string to be completed at end of dest
+*@n:integer parameter to compare index to
+*Return: returns new concatenated string
+*/
+char *_strncat(char *dest, char *src, int n)
 {
-	return (c >= 48 && c <= 57);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
