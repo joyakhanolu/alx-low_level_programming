1a727155ff448675d72ba2b11aaae25de435fb13
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-14 05:13:25 -0700

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
- * _isdigit - checks if a character is digit
- * @c - carrier variable
- * Return: 1 if true, otherwise 0
- */
-
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 int _isdigit(int c)
 {
-	return (c >= 48 && c <= 57);
+if ((c >= 48) && (c <= 57))
+return (1);
+return (0);
 }
