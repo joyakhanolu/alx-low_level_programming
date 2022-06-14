4b39e73a25ce3e9e4b96c49c29fe2a4ae047c9fa
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-14 05:51:53 -0700

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,35 +1,30 @@
 #include "main.h"
-
 /**
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The prefix to be measured.
- *
- * Return: The number of bytes in s which
- * consist only of bytes from accept.
- */
-
+*_strspn - search the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*@s:segment targeted
+*@accept:reference bytes container
+*
+*Return:returns the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int index;
-
-	while (*s)
-	{
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
-
-			else if (accept[index + 1] == '\0')
-				return (bytes);
-		}
-
-		s++;
-	}
-
-	return (bytes);
+unsigned int bytes = 0;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+bytes++;
+break;
+}
+else if ((accept[i + 1]) == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
 }
